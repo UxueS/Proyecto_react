49616f1a1324ba86aa0bb7b9e49d98fffbed61cc
UxueS <usanmigzar@gmail.com>
2025-02-26 16:54:21 +0100

    Header mejorado

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -13,7 +13,6 @@ const Header = () => {
         <ul>
           <li><Link to="#home">Inicio</Link></li>
           <li><Link to="#productos">Productos</Link></li>
-          <li><Link to="#ofertas">Ofertas</Link></li>
           <li><Link to="#contacto">Contacto</Link></li>
         </ul>
       </nav>
