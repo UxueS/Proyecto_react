17a5b2a471a0bf848ee983f5adc18d00a1b2bd67
UxueS <usanmigzar@gmail.com>
2025-03-08 18:59:10 +0100

    Cambios minimos

--- src/App.jsx
+++ src/App.jsx
@@ -38,7 +38,7 @@ function App() {
             setLoading(true);
             setTimeout(() => {
                 setLoading(false);
-            }, 2000);
+            }, 1000);
         }
     }, [location.pathname]);
 
